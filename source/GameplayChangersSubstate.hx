bcc0b45dfa7037391aa0d90664d56d2f030fc604
Joalor64 <99079926+Joalor64GH@users.noreply.github.com>
2022-03-27 15:01:27 -0400

    NOW it should work

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -88,7 +88,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		var option:GameplayOption = new GameplayOption('Practice Mode', 'practice', 'bool', false);
 		optionsArray.push(option);
 
-		var option:GameplayOption = new GameplayOption('Robot Mode', 'robot mode', 'bool', false);
+		var option:GameplayOption = new GameplayOption('Robot Mode', 'botplay', 'bool', false);
 		optionsArray.push(option);
 	}
 
