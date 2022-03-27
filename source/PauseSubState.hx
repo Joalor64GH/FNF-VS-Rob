98d38d07d2cc965e29c9d5afc4e13febf993651a
Joalor64 <99079926+Joalor64GH@users.noreply.github.com>
2022-03-27 13:11:20 -0400

    pause nonsense

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -20,7 +20,7 @@ class PauseSubState extends MusicBeatSubstate
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
 	var menuItems:Array<String> = [];
-	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Exit to menu'];
+	var menuItemsOG:Array<String> = ['Continue', 'Reboot', 'Change Difficulty', 'Log Off'];
 	var difficultyChoices = [];
 	var curSelected:Int = 0;
 
@@ -50,7 +50,7 @@ class PauseSubState extends MusicBeatSubstate
 			}
 			menuItemsOG.insert(3 + num, 'End Song');
 			menuItemsOG.insert(4 + num, 'Toggle Practice Mode');
-			menuItemsOG.insert(5 + num, 'Toggle Botplay');
+			menuItemsOG.insert(5 + num, 'Toggle Robot Mode');
 		}
 		menuItems = menuItemsOG;
 
