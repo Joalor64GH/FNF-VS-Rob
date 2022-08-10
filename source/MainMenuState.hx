5a119143b1fab231ab719a95a22ba1a011774e3d
Joalor64 <rhgarcon@gmail.com>
2022-08-09 22:03:13 -0400

    more stuffs

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -126,7 +126,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-        var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "VS Rob v" + robotVersion, 12);
+        var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "VS Rob v" + robotVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
